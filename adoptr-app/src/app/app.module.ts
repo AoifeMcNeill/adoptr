43325e0740404217e2e2d02d31c2e4553b5d3088
AoifeMcNeill <amcneill849@gmail.com>
2020-04-16 14:55:00 +0100

    added home, about, adopt components

--- adoptr-app/src/app/app.module.ts
+++ adoptr-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { AboutPageComponent } from './components/about-page/about-page.component';
+import { AdoptPageComponent } from './components/adopt-page/adopt-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    AdoptPageComponent
   ],
   imports: [
     BrowserModule,
