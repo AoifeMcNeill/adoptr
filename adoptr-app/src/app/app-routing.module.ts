18f4c817a386bb317de455a024bd0fe73490418e
AoifeMcNeill <amcneill849@gmail.com>
2020-04-16 15:09:35 +0100

    added components to app-routing.module.ts

--- adoptr-app/src/app/app-routing.module.ts
+++ adoptr-app/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {HomePageComponent} from './components/home-page/home-page.component';
+import {AdoptPageComponent} from './components/adopt-page/adopt-page.component';
+import {AboutPageComponent} from './components/about-page/about-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home-page' },
+  { path: 'home-page', component: HomePageComponent },
+  { path: 'adopt-page', component: AdoptPageComponent },
+  { path: 'about-page', component: AboutPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
