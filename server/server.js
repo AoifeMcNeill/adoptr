0d2c15b7a1e3065f82b6ff60ff330e8d1fd663c8
AoifeMcNeill <amcneill849@gmail.com>
2020-04-16 12:24:21 +0100

    server.js page created

---
+++ server/server.js
@@ -0,0 +1,6 @@
+let express = require('express'),
+path = require('path'),
+mongoose = require('mongoose'),
+bodyParser = require('body-parser'),
+cors = require('cors'),
+dbConfig = require('./database/db');
