8f5ac4f1fe606001e6f4e4295fc44837b27daf4e
AoifeMcNeill <amcneill849@gmail.com>
2020-04-16 12:30:28 +0100

    add mongoose connection to server.js

--- server/server.js
+++ server/server.js
@@ -4,3 +4,13 @@ mongoose = require('mongoose'),
 bodyParser = require('body-parser'),
 cors = require('cors'),
 dbConfig = require('./database/db');
+
+mongoose.connect(dbConfig.db, {
+    useNewUrlParser: true
+}).then(() => {
+        console.log('Database successfully connected')
+    },
+    error => {
+        console.log('Database could not connect:' + error)
+    }
+)
