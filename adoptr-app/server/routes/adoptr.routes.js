13db7e041348e75e37eea87cd1e167cc19afc72d
AoifeMcNeill <amcneill849@gmail.com>
2020-04-19 16:00:26 +0100

    updatd adoptr.routes.js

--- adoptr-app/server/routes/adoptr.routes.js
+++ adoptr-app/server/routes/adoptr.routes.js
@@ -6,7 +6,7 @@ const adoptrRoute = express.Router();
 let List = require('../models/adoptr');
 
 //Get all pets
-adoptrRoute.route('/').get((req, res) => {
+adoptrRoute.route('/').get((req, res, next) => {
     List.find((error, data) => {
         if (error) {
             return next(error)
@@ -17,7 +17,7 @@ adoptrRoute.route('/').get((req, res) => {
 })
 
 //Get one pet by id
-adoptrRoute.route('/read/:id').get((req, res) => {
+adoptrRoute.route('/read/:id').get((req, res, next) => {
     List.findById(req.params.id, (error, data) => {
         if (error) {
             return next(error)
@@ -28,7 +28,7 @@ adoptrRoute.route('/read/:id').get((req, res) => {
 })
 
 //Get pets by category
-adoptrRoute.route('/category/:category').get((req, res) => {
+adoptrRoute.route('/category/:category').get((req, res, next) => {
     List.find({category: req.params.category}, (error, data) => {
         if (error) {
             return next(error)
@@ -39,7 +39,7 @@ adoptrRoute.route('/category/:category').get((req, res) => {
 })
 
 //Get pets by breed
-adoptrRoute.route('/breed/:breed').get((req, res) => {
+adoptrRoute.route('/breed/:breed').get((req, res, next) => {
     List.find({breed: req.params.breed}, (error, data) => {
         if (error) {
             return next(error)
@@ -50,7 +50,7 @@ adoptrRoute.route('/breed/:breed').get((req, res) => {
 })
 
 //Get pets by colour
-adoptrRoute.route('/colour/:colour').get((req, res) => {
+adoptrRoute.route('/colour/:colour').get((req, res, next) => {
     List.find({colour: req.params.colour}, (error, data) => {
         if (error) {
             return next(error)
@@ -61,7 +61,7 @@ adoptrRoute.route('/colour/:colour').get((req, res) => {
 })
 
 //Get pets by coat
-adoptrRoute.route('/coat/:coat').get((req, res) => {
+adoptrRoute.route('/coat/:coat').get((req, res, next) => {
     List.find({coat: req.params.coat}, (error, data) => {
         if (error) {
             return next(error)
@@ -72,7 +72,7 @@ adoptrRoute.route('/coat/:coat').get((req, res) => {
 })
 
 //Get pets by size
-adoptrRoute.route('/size/:size').get((req, res) => {
+adoptrRoute.route('/size/:size').get((req, res, next) => {
     List.find({size: req.params.size}, (error, data) => {
         if (error) {
             return next(error)
@@ -83,7 +83,7 @@ adoptrRoute.route('/size/:size').get((req, res) => {
 })
 
 //Get pets by sex
-adoptrRoute.route('/sex/:sex').get((req, res) => {
+adoptrRoute.route('/sex/:sex').get((req, res, next) => {
     List.find({sex: req.params.sex}, (error, data) => {
         if (error) {
             return next(error)
@@ -94,7 +94,7 @@ adoptrRoute.route('/sex/:sex').get((req, res) => {
 })
 
 //Get pets by age
-adoptrRoute.route('/age/:age').get((req, res) => {
+adoptrRoute.route('/age/:age').get((req, res, next) => {
     List.find({age: req.params.age}, (error, data) => {
         if (error) {
             return next(error)
@@ -105,7 +105,7 @@ adoptrRoute.route('/age/:age').get((req, res) => {
 })
 
 //Get pets by cat_friendly
-adoptrRoute.route('/cat_friendly/:cat_friendly').get((req, res) => {
+adoptrRoute.route('/cat_friendly/:cat_friendly').get((req, res, next) => {
     List.find({cat_friendly: req.params.cat_friendly}, (error, data) => {
         if (error) {
             return next(error)
@@ -116,7 +116,7 @@ adoptrRoute.route('/cat_friendly/:cat_friendly').get((req, res) => {
 })
 
 //Get pets by dog_friendly
-adoptrRoute.route('/dog_friendly/:dog_friendly').get((req, res) => {
+adoptrRoute.route('/dog_friendly/:dog_friendly').get((req, res, next) => {
     List.find({dog_friendly: req.params.dog_friendly}, (error, data) => {
         if (error) {
             return next(error)
@@ -127,7 +127,7 @@ adoptrRoute.route('/dog_friendly/:dog_friendly').get((req, res) => {
 })
 
 //Get pets by child_friendly
-adoptrRoute.route('/child_friendly/:child_friendly').get((req, res) => {
+adoptrRoute.route('/child_friendly/:child_friendly').get((req, res, next) => {
     List.find({child_friendly: req.params.child_friendly}, (error, data) => {
         if (error) {
             return next(error)
@@ -138,7 +138,7 @@ adoptrRoute.route('/child_friendly/:child_friendly').get((req, res) => {
 })
 
 //Get pets by adoptable
-adoptrRoute.route('/adoptable/:adoptable').get((req, res) => {
+adoptrRoute.route('/adoptable/:adoptable').get((req, res, next) => {
     List.find({adoptable: req.params.adoptable}, (error, data) => {
         if (error) {
             return next(error)
